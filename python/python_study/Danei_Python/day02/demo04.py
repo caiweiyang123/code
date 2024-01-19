f883e462ccd9ead0053e738aa10ed4f59777487f
zhengyilai <zhengyilai@lixiang.com>
2024-01-19 19:40:18 +0800

    bugfix

--- python/python_study/Danei_Python/day02/demo04.py
+++
@@ -1,6 +0,0 @@
-import module01
-
-module01.fun01()
-
-my02 = module01.Myclass01()
-my02.fun02()
--- python/python_study/Danei_Python/day02/module01.py
+++
@@ -1,8 +0,0 @@
-print("模块1")
-
-def fun01():
-    print("模块1的fun01函数")
-
-class Myclass01:
-    def fun02(self):
-        print("Myclass01中的fun02")
