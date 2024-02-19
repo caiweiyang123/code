022c41e49b0915e217a15ee60b6b82b1fe1a3e8f
invest_cai_80694 <invest_cai@163.com>
2024-02-19 14:01:25 +0800

    test

--- python/ceshi.py
+++ python/ceshi.py
@@ -1,7 +1,1 @@
-import numpy as np
-teacher = np.dtype([('name','S20'), ('age', 'i1'), ('salary', 'f4')])
-#输出结构化数据teacher
-print(teacher)
-#将其应用于ndarray对象
-b = np.array([('ycs', 32, 6357.50),('jxe', 28, 6856.80)], dtype = teacher)
-print(b)
+print(125)
